b11403236d8b9c0c39cdcfe551a746de14ba71d3
Pedro Henrique Mattiello <Pedromattiello@gmail.com>
2019-05-15 23:32:28 -0300

    adiçao de limiarizaçao e segmentaçao

--- src/application/ProcessadorImagemController.java
+++ src/application/ProcessadorImagemController.java
@@ -4,12 +4,24 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Range;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -26,7 +38,6 @@ public class ProcessadorImagemController implements Initializable {
 	private Button open_btn;
     
     private File arquivo_imagem;
-    
     
 	public void abrir_imagem() throws FileNotFoundException {
 		System.out.println("abriu");
@@ -87,7 +98,75 @@ public class ProcessadorImagemController implements Initializable {
 		}catch(Exception e ) {
 			//@todo mostrar msg na tela
 		}
+	}
+	
+	public void limiarizar() { 
+		Mat image = Imgcodecs.imread(arquivo_imagem.getAbsolutePath(), Imgcodecs.IMREAD_GRAYSCALE);  
+		Mat kernel = Mat.ones(1, 3, CvType.CV_8U);
+		Imgproc.morphologyEx(image, image, Imgproc.MORPH_BLACKHAT, kernel, new Point(1,0));
+		Image imageToShow = Utils.mat2Image(image);
+		updateImageView(img_carregada, imageToShow);	
+	}
+	
+	public void segmentar() { 
+		Mat image = Imgcodecs.imread(arquivo_imagem.getAbsolutePath(), Imgcodecs.IMREAD_GRAYSCALE);
 		
+		double scale =   800.0 / image.cols();
+		Mat resizeImage = new Mat(( int) (image.rows() * scale), (int) (image.cols() * scale), image.type());
+	    Imgproc.resize(image, image, resizeImage.size());
+	    
+	    new Mat();
+		Mat kernel = Mat.ones(1, 3, CvType.CV_8U);
+		System.out.println(kernel.dump());
+	    
+		Imgproc.morphologyEx(image, image, Imgproc.MORPH_BLACKHAT, kernel, new Point(1,0));
+		Imgproc.threshold(image, image, 10, 255, Imgproc.THRESH_BINARY);
+		
+		kernel = Mat.ones(1, 5, CvType.CV_8U);
+		Imgproc.morphologyEx(image, image, Imgproc.MORPH_DILATE, kernel, new Point(2,0), 2);
+		Imgproc.morphologyEx(image, image, Imgproc.MORPH_CLOSE, kernel,  new Point(2,0), 2);
+		
+		kernel = Mat.ones(21, 35, CvType.CV_8U);
+		Imgproc.morphologyEx(image, image, Imgproc.MORPH_OPEN, kernel, new Point(-1,-1), 1);
+		
+		//
+		Mat image_out = Imgcodecs.imread(arquivo_imagem.getAbsolutePath());
+		List<MatOfPoint> contours = new ArrayList<>();
+		Mat hierarchy = new Mat();
+		Imgproc.findContours(image, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+		
+		double unscale = 1.0 / scale;
+		if(contours != null) { 
+			for(int index=0;index < contours.size();index++) { 
+				if(Imgproc.contourArea(contours.get(index)) <= 2000) { 
+					continue; // skip interation
+				}
+				
+				MatOfPoint contour = contours.get(index);
+				MatOfPoint2f  NewMtx = new MatOfPoint2f( contour.toArray() );
+				
+				RotatedRect rect = Imgproc.minAreaRect(NewMtx);
+				
+				Point[] p = new Point[4];
+				rect.points(p);	
+				for(int pointIdx =0; pointIdx < 4;pointIdx++) { 
+					p[pointIdx].x = p[pointIdx].x * unscale;
+					p[pointIdx].y = p[pointIdx].y * unscale;
+				}	
+				MatOfPoint mp = new MatOfPoint(p);
+
+				Imgproc.drawContours(image_out, Arrays.asList(mp), 0, new Scalar(255,0,0), 2);
+			}
+		}
+		Image imageToShow = Utils.mat2Image(image_out);
+		updateImageView(img_carregada, imageToShow);	
+	}
+	
+	// printar/testar os numeros da matrix 
+	private void showMatrixInRange(Mat image, Range range) {
+		int [] buffer = new int[(int) image.total() * image.channels()];
+		Mat ROU = image.submat(range, range);
+		System.out.println(ROU.dump().replace("0,   ", ""));
 	}
 	
 	private void set_image(File file) throws FileNotFoundException {
